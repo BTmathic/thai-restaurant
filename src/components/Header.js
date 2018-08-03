fb0902c9618a95f8a44ff5ba8e8c877788a487f3
Alexander Molnar <mathic@gmail.com>
2018-08-03 16:20:08 -0400

    Fixed header links to stay local

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ export default () => (
   <div className='header'>
     <header>
       <div className='header__content'>
-        <a href='/#top'>HOME</a>
-        <a href='/#about'>ABOUT US</a>
-        <a href='/#menu'>MENU</a>
-        <a href='/#contact'>CONTACT</a>
+        <a href='#top'>HOME</a>
+        <a href='#about'>ABOUT US</a>
+        <a href='#menu'>MENU</a>
+        <a href='#contact'>CONTACT</a>
       </div>
     </header>
     <img src='/Images/royal-angkor.jpg' alt='restaurant logo' className='test'/>
